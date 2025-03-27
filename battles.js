e75921504982add7a0839e1b0bdbff7dfb928a09
oGroovy <76412774+ItsMrRedboy19@users.noreply.github.com>
2025-03-27 16:17:20 +1300

    put the battle locations in another file

---
+++ battles.js
@@ -0,0 +1,26 @@
+const battles = {
+    Wellington: {
+        coords: [-41.2865, 174.7762],
+        info: `<div class="popup-content">
+                <h3>Wellington</h3>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/HekeFlagstaff.jpg/390px-HekeFlagstaff.jpg" alt="description" />
+                <p>Wellington is the capital city of New Zealand, known for its vibrant culture, windy weather, and stunning harbor views.</p>
+            </div>`
+    },
+    Auckland: {
+        coords: [-36.8485, 174.7633],
+        info: `<div class="popup-content">
+                <h3>Auckland</h3>
+                <img src="https://upload.wikimedia.org/wikipedia/en/thumb/f/f9/Waireka.jpg/350px-Waireka.jpg" alt="Auckland">
+                <p>Auckland is known as the City of Sails, famous for its harbors, bustling urban life, and volcanic cones.</p>
+            </div>`
+    },
+    Christchurch: {
+        coords: [-43.5321, 172.6362],
+        info: `<div class="popup-content">
+                <h3>Christchurch</h3>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Von_Tempsky%27s_death_Kennett_Watkins.jpg/1200px-Von_Tempsky%27s_death_Kennett_Watkins.jpg" alt="Christchurch">
+                <p>Christchurch is in the South Island, celebrated for its innovative architecture, gardens, and English heritage.</p>
+            </div>`
+    }
+};
