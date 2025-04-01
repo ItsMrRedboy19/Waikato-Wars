cc16531cef3306759da03c2004b38b0d6ad0877c
oGroovy <76412774+ItsMrRedboy19@users.noreply.github.com>
2025-04-01 16:25:29 +1300

    added the battles of the Taranaki War

--- battles.js
+++ battles.js
@@ -1,47 +1,56 @@
 const battles = {
-  Kororareka: {
-    coords: [-35.263642903151045, 174.12197907166885],
-    info: `<div class="popup-content">
-            <h3>Flagpole Cut Down</h3>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/HekeFlagstaff.jpg/390px-HekeFlagstaff.jpg" alt="Russell" />
-            <a href="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/HekeFlagstaff.jpg/390px-HekeFlagstaff.jpg">Source: Wikimedia Commons</a>
-            <p>placeholder</p>
-        </div>`
-  },
-  Puketutu: {
-    coords: [-35.33873644832787, 173.8173143049554],
-    info: `<div class="popup-content">
-            <h3>Battle of Puketutu</h3>
-            <img src="https://teara.govt.nz/files/36898-atl.jpg" alt="Battle of Puketutu">
-            <a href="https://teara.govt.nz/files/36898-atl.jpg">Source: Te Ara Encyclopedia</a>
-            <p>placeholder</p>
-        </div>`
-  },
-  Ohaeawai: {
-    coords: [-35.37555258686437, 173.85538167222242],
-    info: `<div class="popup-content">
-            <h3>Battle of Ohaeawai</h3>
-            <img src="https://nzhistory.govt.nz/sites/default/files/styles/wide/public/images/ohaeawai-pa.jpg?itok=4gH524M0" alt="Battle of Ohaeawai">
-            <a href="https://nzhistory.govt.nz/sites/default/files/styles/wide/public/images/ohaeawai-pa.jpg?itok=4gH524M0">Source: NZ History</a>
-            <p>placeholder</p>
-        </div>`
-  },
-  Ruapekapeka: {
-    coords: [-35.456389, 174.143611],
-    info: `<div class="popup-content">
-            <h3>Battle of Ruapekapeka</h3>
-            <img src="https://static.wixstatic.com/media/836e27_0d85ba0cf76a49c68948a4de47acbf98~mv2.png/v1/fill/w_740,h_457,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/836e27_0d85ba0cf76a49c68948a4de47acbf98~mv2.png" alt="Battle of Ruapekapeka">
-            <a href="https://static.wixstatic.com/media/836e27_0d85ba0cf76a49c68948a4de47acbf98~mv2.png/v1/fill/w_740,h_457,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/836e27_0d85ba0cf76a49c68948a4de47acbf98~mv2.png">Source: Royal Marines History</a>
-            <p>placeholder</p>
-        </div>`
-  },   
-  Te_Kohia: {
-    coords: [-39.06578745714039, 174.26363612230682],
-    info: `<div class="popup-content">
-            <h3>Battle of Te Kohia</h3>
-            <img src="temp" alt="Battle of Te Kohia">
-            <a href="temp">Source: temp</a>
-            <p>placeholder</p>
-        </div>`
-},   
-};
+    Kororareka: {
+      coords: [-35.263642903151045, 174.12197907166885],
+      info: `<div class="popup-content">
+              <h3>Flagpole Cut Down</h3>
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/HekeFlagstaff.jpg/390px-HekeFlagstaff.jpg" alt="Russell" />
+              <a href="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/HekeFlagstaff.jpg/390px-HekeFlagstaff.jpg">Source: Wikimedia Commons</a>
+              <p>placeholder</p>
+          </div>`
+    },
+    Puketutu: {
+      coords: [-35.33873644832787, 173.8173143049554],
+      info: `<div class="popup-content">
+              <h3>Battle of Puketutu</h3>
+              <img src="https://teara.govt.nz/files/36898-atl.jpg" alt="Battle of Puketutu">
+              <a href="https://teara.govt.nz/files/36898-atl.jpg">Source: Te Ara Encyclopedia</a>
+              <p>placeholder</p>
+          </div>`
+    },
+    Ohaeawai: {
+      coords: [-35.37555258686437, 173.85538167222242],
+      info: `<div class="popup-content">
+              <h3>Battle of Ohaeawai</h3>
+              <img src="https://nzhistory.govt.nz/sites/default/files/styles/wide/public/images/ohaeawai-pa.jpg?itok=4gH524M0" alt="Battle of Ohaeawai">
+              <a href="https://nzhistory.govt.nz/sites/default/files/styles/wide/public/images/ohaeawai-pa.jpg?itok=4gH524M0">Source: NZ History</a>
+              <p>placeholder</p>
+          </div>`
+    },
+    Ruapekapeka: {
+      coords: [-35.456389, 174.143611],
+      info: `<div class="popup-content">
+              <h3>Battle of Ruapekapeka</h3>
+              <img src="https://static.wixstatic.com/media/836e27_0d85ba0cf76a49c68948a4de47acbf98~mv2.png/v1/fill/w_740,h_457,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/836e27_0d85ba0cf76a49c68948a4de47acbf98~mv2.png" alt="Battle of Ruapekapeka">
+              <a href="https://static.wixstatic.com/media/836e27_0d85ba0cf76a49c68948a4de47acbf98~mv2.png/v1/fill/w_740,h_457,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/836e27_0d85ba0cf76a49c68948a4de47acbf98~mv2.png">Source: Royal Marines History</a>
+              <p>placeholder</p>
+          </div>`
+    },   
+    Waireka: {
+      coords: [-39.09240359, 174.00530270],
+      info: `<div class="popup-content">
+              <h3>Battle of Waireka</h3>
+              <img src="https://nzhistory.govt.nz/sites/default/files/styles/wide/public/images/waireka-cowan.jpg?itok=uY_u_jh3" alt="Battle of Waireka">
+              <a href="https://nzhistory.govt.nz/sites/default/files/styles/wide/public/images/waireka-cowan.jpg?itok=uY_u_jh3">Source: NZ History</a>
+              <p>placeholder</p>
+          </div>`
+    },
+    Puketakauere: {
+      coords: [-39.01651453, 174.23017290],
+      info: `<div class="popup-content">
+              <h3>Battle of Puketakauere</h3>
+              <img src="https://nzhistory.govt.nz/sites/default/files/styles/nzhistory_thumbnail_portrait/public/images/taranaki-war-icon.jpg" alt="Battle of Puketakauere">
+              <a href="https://nzhistory.govt.nz/sites/default/files/styles/nzhistory_thumbnail_portrait/public/images/taranaki-war-icon.jpg">Source: NZ History</a>
+              <p>placeholder</p>
+          </div>`
+    },    
+  };
